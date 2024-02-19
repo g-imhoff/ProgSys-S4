ccbb935f3dcbaf1a610441b37bc27d239d670c46
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2024-02-19 14:56:05 +0100

    cp_rev est fini et marche !

---
+++ TP1/ex4/header/cp_rev.h
@@ -0,0 +1,6 @@
+#ifndef CP_REV_H
+#define CP_REV_H
+
+void cp_rev(const char* filepath1, const char* filepath2);
+
+#endif
---
+++ TP1/ex4/src/main.c
@@ -0,0 +1,13 @@
+#include "base.h"
+#include "cp_rev.h"
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        printf("Ce programme nécessite 4 arguments mais a reçu : %d\n", argc);
+        exit(EXIT_FAILURE);
+    }
+    
+    cp_rev(argv[1], argv[2]); 
+    
+    return EXIT_SUCCESS;
+}
